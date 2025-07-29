a6cc771b09e457538635fe4cc3527a0307b8f97e
RUDYMAZE <njockrudiger4@gmail.com>
2025-07-29 21:50:47 +0300

    fix bugs

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Layout from "@/components/layouts/Layout";
+import { CountProvider } from "@/context/CountContext";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <CountProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CountProvider>
+  );
 }
